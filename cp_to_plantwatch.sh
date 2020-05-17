3600069e5f678842b15bc1d0357be8e0816502f8
Simon Dietz <simon.dietz@student.hpi.de>
2020-05-17 16:16:58 +0200

    clean up repo

--- cp_to_plantwatch.sh
+++ cp_to_plantwatch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp -f blocks_nh.csv ../plantwatch/db/blocks_nh.csv
-cp -f stammdaten_nh.csv ../plantwatch/db/stammdaten_nh.csv
+cp -f blocks_nh.csv ../plantwatch2/db/blocks_nh.csv
+cp -f stammdaten_nh.csv ../plantwatch2/db/stammdaten_nh.csv
