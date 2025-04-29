0f8a4c1345985cc1bb15c50c2a57cb0ecb2c87e2
sidietz <simon.dietz@plantwatch.de>
2025-04-29 19:53:52 +0200

    money money money

---
+++ money/helpers.py
@@ -0,0 +1,19 @@
+def get_price(x):
+    if x == "Erdgas":
+        return 40
+    elif x == "Steinkohle":
+        return 120
+    elif x == "Braunkohle":
+        return 18
+    else:
+        return 75
+
+def get_factor(x):
+    if x == "Erdgas":
+        return 1.5
+    elif x == "Steinkohle":
+        return 2.68
+    elif x == "Braunkohle":
+        return 3.25
+    else:
+        return 2.3
