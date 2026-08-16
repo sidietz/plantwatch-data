0ed9b8fe22e23daec4fa5e36c43302f357767e92
Simon Dietz <simon.dietz@plantwatch.de>
2026-08-16 18:50:38 +0200

    prepare for plant list 2026

--- basic/helpers.py
+++ basic/helpers.py
@@ -6,6 +6,9 @@ names_dict = {'MaStR-Nr. der Stromerzeugungseinheit': "blockid",  'Kraftwerksnum
 
 names_dict_retired = {'MaStR-Nr. der Stromerzeugungseinheit': "blockid",  'Kraftwerksnummer der Bundesnetzagentur': "bnaid", 'Anlagenbetreiber': "company", 'Anzeige-Name der Stromerzeugungseinheit': "plantname", 'PLZ der Einheit': "plz", 'Ort der Einheit': "place", 'Straße der Einheit': "street", 'Hausnummer der Einheit': "streetnum", 'Bundesland der Einheit': "federalstate", 'Datum der erstmaligen Inbetriebnahme der Einheit (Datum/Jahr)': "initialop", 'Datum der endgültigen Stilllegung der Einheit (Datum/Jahr)': "endop", 'Kraftwerksstatus der Einheit': "state", 'Energieträger': "e1", 'Hauptbrennstoff': "e2", 'Auswertung Energieträger': "energysource", 'Förderberechtigt nach EEG\n(ja/nein)': "eeg", 'Wärmeauskopplung (KWK)\n(ja/nein)': "chp", 'Erneuerbarer Energieträger\n(ja/nein)': "ee", 'Ist die Stromerzeugungseinheit ein Bestandteil eines Grenzkraftwerkes?': "border", 'Bruttoleistung in MW': "grosspower", 'Nettonennleistung (elektrische Wirkleistung) in MW': "power", 'Ist die Stromerzeugungseinheit ein Bestandteil eines Grenzkraftwerkes?: ja \nNettonennleistung der Einspeisung in ein deutsches Netz:': "gerpower", 'Technologie der Stromerzeugung': "tech", 'Volleinspeisung oder Teileinspeisung?': "fullsupply", 'Anschlussnetzbetreiber': "TSO", 'Spannungsebene': "voltagelevel", 'Wenn Energieträger Speicher: Nutzbare Speicherkapazität in Mwh': "storage"}
 
+names_dict4 = {'Datensatztyp*': 'datatype', 'EinheitMastrNummer': "blockid", 'Anlagenbetreiber': "company", 'Anzeigename': "plantname", 'Postleitzahl': "plz", 'Ort': "place", 'Strasse': "street", 'Hausnummer': "streetnum", 'Bundesland': "federalstate", 'Land': 'nation', 'Jahr_Inbetriebnahme': 'initialopYear', 'Jahr_Stilllegung**': 'endop', 'Kraftwerksstatus': "state", 'Energietraeger': "energysource", 'Hauptbrennstoff': "e2", 'Waermeauskopplung_KWK': "chp", 'Erneuerbarer_Energietraeger': "eeg", 'Bruttoleistung_MW': "grosspower", 'Nettonennleistung_MW': "power", 'Technologie_Stromerzeugung': "tech", 'Volleinspeisung_Teileinspeisung': "fullsupply", 'Spannungsebene': "voltagelevel", 'Anschlussnetzbetreiber': "TSO", 'Bestandteil_Grenzkraftwerk': 'border', 'Grenzkraftwerk_Nettonennleistung_MW': "gerpower"}
+
+
 def get_renamed_blocks2(pl0):
     pl0nd2 = {}
     for idx, new, old in list(zip(pl0nd1.keys(), pl0nd1.values(), list(pl0))):
@@ -19,6 +22,10 @@ def get_renamed_blocks(pl0):
 
 def get_renamed_blocks3(pl0):
     plt = pl0.rename(columns=names_dict_retired)
+    return plt
+
+def get_renamed_blocks4(pl0):
+    plt = pl0.rename(columns=names_dict4)
     return plt
 
 def match_inspire(name):
